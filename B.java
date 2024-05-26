d9b98facfbbd778186f998511a7d83e5bdaf34e1
nayan malviya <naymalviya@gmail.com>
2024-05-26 09:27:17 +0530

    Vector for String

---
+++ B.java
@@ -0,0 +1,14 @@
+import java.util.Vector;
+
+class B{
+    public static void main(String[] args){
+        Vector<String> str = new Vector<String>();
+
+        str.add("ironman");
+        str.add("shaktiman");
+        str.add("flash");
+        str.add("captan america");
+
+        System.out.println(str);
+    }
+}
