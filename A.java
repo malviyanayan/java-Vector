670b53debd5a1f061d6f12d9f739f0c3904bcc12
nayan malviya <naymalviya@gmail.com>
2024-05-26 09:17:38 +0530

    Vector class in java

---
+++ A.java
@@ -0,0 +1,16 @@
+import java.util.Vector;
+
+// this is Vector class
+
+public class A{
+    public static void main(String[] args){
+        Vector a = new Vector();
+        // new Vector object
+
+        a.add(10);
+        a.add(20);
+        a.add(30);
+
+        System.out.println(a);
+    }
+}
